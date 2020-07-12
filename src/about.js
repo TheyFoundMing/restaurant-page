73ef6918e3380580aac4ab6b359ed5d051819768
Michaella Magtibay <michaeltmagtibay@gmail.com>
2020-07-12 15:07:33 +0700

    Add render() and the basic materials needed

--- src/about.js
+++ src/about.js
@@ -1,4 +1,28 @@
 
-const aboutSection = ({
+const aboutSection = (function (){
+
+    function render() {
+        let content = document.querySelector('#actual-content');
+
+        let title = document.createElement('h1');
+        let first = document.createElement('p');
+        let second = document.createElement('p');
+        let mission = document.createElement('p');
     
-})()
+        title.innerText = 'About';
+        first.innerText = "The idea of German Pork Knuckle inspired the founding partners to unlock the full potential of Pork Knuckle as an ingredient. So back in 2015, after months of experimenting and perfecting the recipe, they discovered a Pork Knuckle dish that taste so good, they knew they had to share it right away. As time goes on, the partners found out that the demand for our food increased from day to day basis and that is when the Pork Knuckle over rice platter was born. Ok, let's not forget about the awesome Sambal Matah and Sambal Hijau!";
+        second.innerText = "This website was created by TheyFoundMing (Michaella) in order to improve on my skills, HogHock is indeed a real restaurant located in Indonesia and I truly enjoy eating there.";
+        mission.innerText = "Our main mission is continuously improve the quality of our food and services in order to exceed customers increasing expectations.";
+    
+        content.appendChild(title);
+        content.appendChild(first);
+        content.appendChild(second);
+        content.appendChild(mission);
+    }
+
+    return {render}; 
+})();
+
+// aboutSection.render();
+
+export { aboutSection };
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,45 @@
 
-const menuSection = ({
-    
-})()
+const menuSection = (function () {
+ 
+    function render() {
+
+        let foodnames = ['Pork Knuckle Original', 'Pork Knuckle Matah', 'BBQ Pork Belly'];
+        let descriptions = ['Stir-Fry Tender Pork Knuckle served with rice, crispy pork skin and special green chili.',
+                'Stir-Fry Tender Pork Knuckle served with rice, crispy pork skin and special matah condiment.',
+                'BBQ Pork Belly served with rice and special green chili.'];
+
+        let content = document.querySelector('#actual-content');
+        let menu = document.createElement('div');
+
+        let PorkBelly = document.createElement('img');
+        let PorkKnuckle = document.createElement('img');
+
+        PorkBelly.src = 'img/PorkBelly.jpg';
+        PorkKnuckle.src = 'img/PorkKnuckle.jpg';
+
+        menu.append(PorkBelly, PorkKnuckle);
+
+        for (let i=0; i<foodnames.length; i++) {
+            let food = document.createElement('div');
+            let name = document.createElement('h5');
+            let desc = document.createElement('span')
+
+            food.classList.add('food');
+            name.classList.add('food-name');
+            desc.classList.add('food-desc');
+
+            name.innerText = foodnames[i];
+            desc.innerText = descriptions[i];
+
+            food.appendChild(name);
+            food.appendChild(desc);
+            menu.appendChild(food);
+        }
+
+        content.appendChild(menu);
+    }
+
+    return  {render}
+})();
+
+export { menuSection };
